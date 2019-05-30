2e84ec0404f953e478a313b190d1a698cc318f99
Deiziane <deiziane_ns@hotmail.com>
2019-05-30 20:58:35 -0300

    generating orange ppm

--- TP2/raytracer.py
+++ TP2/raytracer.py
@@ -1,43 +1,45 @@
 import sys
 import numpy as np
+import array
 from math import sqrt, pow, pi
 
-# stores 3 values 
-class Vec3( object ):
-	def __init__(self, x, y, z):
-		self.x = x
-		self.y = y
-		self.z = z
+# stores 3 values
+class Vec3(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
     # vector dot product
-	def dot(self, number): 
-		return self.x*number.x + self.y*number.y + self.z*number.z
+    def dot(self, number):
+        return self.x*number.x + self.y*number.y + self.z*number.z
 
     # vector cross product
-	def cross(self, number):  
-		return (self.y*number.z - self.z*number.y, self.z*number.x - self.x*number.z, self.x*number.y - self.y*number.x)
+    def cross(self, number):
+        return (self.y*number.z - self.z*number.y, self.z*number.x - self.x*number.z, self.x*number.y - self.y*number.x)
 
     # vector magnitude
-	def magnitude(self): 
-		return sqrt(self.x**2+self.y**2+self.z**2)
+    def magnitude(self):
+        return sqrt(self.x**2+self.y**2+self.z**2)
 
-    # compute a normalized vector
-	def normalize(self): 
-		mag = self.magnitude()
-		return Vec3(self.x/mag, self.y/mag, self.z/mag)
+    @staticmethod
+    def normalize(self):
+        # compute a normalized vector
+        mag = self.magnitude()
+        return Vec3(self.x/mag, self.y/mag, self.z/mag)
 
     # sum vectors
-	def __add__(self, number):  
-		return Vec3(self.x + number.x, self.y+number.y, self.z+number.z)
+    def __add__(self, number):  
+        return Vec3(self.x + number.x, self.y+number.y, self.z+number.z)
 
     # subtract vectors
-	def __sub__(self, number):  
-		return Vec3(self.x - number.x, self.y - number.y, self.z - number.z)
+    def __sub__(self, number):  
+        return Vec3(self.x - number.x, self.y - number.y, self.z - number.z)
 
     # multiply vector by scalar
-	def __mul__(self, number):  
-		assert type(number) == float or type(number) == int
-		return Vec3(self.x*number, self.y*number, self.z*number)
+    def __mul__(self, number):  
+        assert type(number) == float or type(number) == int
+        return Vec3(self.x*number, self.y*number, self.z*number)
 
 
 '''
@@ -46,11 +48,12 @@ compute what color is seen in the direction of those rays.
 '''
 class Ray:
     def __init__(self, origin, direction):
-        self.o = origin
-        self.d = direction
+        self.origin = origin
+        self.direction = direction
 
     def point_at_parameter(self, t):
         return self.origin + self.direction * t
+
 
 
 '''
@@ -64,9 +67,10 @@ class Sphere:
 
 
 def color(r):
-    u_direction = Vec3()
-    u_direction = u_direction.normalize(r)
-
+    u_direction = Vec3(0,0,0)
+    u_direction = Vec3.normalize(r.direction)
+    t = 0.5*(u_direction.y + 1.0)
+    return Vec3(1.0, 1.0, 1.0).__mul__((1.0*t)).__add__(Vec3(0.5, 0.7, 1.0).__mul__(t))
 
 '''
 Trace rays 
@@ -97,18 +101,45 @@ def main():
     # default image size
     width = 340
     height = 480
+    output = None
 
     # reads output file name
-    if(sys.argv < 1):
+    if(len(sys.argv) < 2):
         print("Forneça o nome do arquivo de saída!")
+        sys.exit()
     else:
         output = sys.argv[1] 
 
     # user can optionally specify width and height
-    if(sys.argv > 2):
+    if(len(sys.argv) > 2):
         width = sys.argv[2]
         height = sys.argv[3]
 
+    ppm_header = "P6 "+ str(width) +" "+ str(height) +" 255\n"
+    image = array.array('B', [0, 0, 0] * width * height)
+
+    lower_left_corner = Vec3(-2.0, -1.0, -1.0)
+    horizontal = Vec3(4.0, 0.0, 0.0)
+    vertical = Vec3(0.0, 2.0, 0.0)
+    origin = Vec3(.0, .0, .0)
+
+    for j in range(height-1, -1, -1):
+        for i in range(0, width):
+            u = i/width
+            v = j/height
+            r = Ray(origin, lower_left_corner.__add__(horizontal.__mul__(u).__add__(vertical.__mul__(v))))
+            col = Vec3(color(r).x, color(r).y, color(r).z)
+            index = 3 * (j * width + i)
+            image[index] = int(255)           # red channel
+            image[index + 1] = int(155)         # green channel
+            image[index + 2] = int(55)         # blue channel
+          
+
+
+    # Save the PPM image as a binary file
+    with open(output, 'wb') as f:
+        f.write(bytearray(ppm_header, 'ascii'))
+        image.tofile(f)
 
 
 if __name__ == '__main__':
